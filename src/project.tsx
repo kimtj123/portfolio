e06fa027c91d34e01bb6a153140680f3f305c4bb
kimtj123 <kimtajung1@gmail.com>
2020-03-13 00:21:57 +0900

    카드 하단 버튼 추가, 카드 내용 반영, About me 구성 변경

--- src/project.tsx
+++ src/project.tsx
@@ -3,8 +3,6 @@ import './scss/project.scss';
 import ProjectList from './sub-component/project-list'
 import Card from './sub-component/card'
 
-import { faLink } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faBlogger } from "@fortawesome/free-brands-svg-icons";
 
 import MozzaBoard from './img/mozza-board.gif';
 import MozzaDelete from './img/mozza-deletelist.gif';
@@ -23,44 +21,44 @@ interface detailProps {
   term: string,
   stack: string[],
   comment: string,
-  link: [string, object][];
+  link: string[],
   gif: string[]
 }
 
 function Project() {
   let projectInfo: projectProps = {
     mozzarello: {
-      title: "Mozzarello",
       term: "2019.12 ~ 2019.01 / 5주",
-      stack: ["Javascript", "React", "HTML", "CSS", "MongoDB", "Mongoose(koa)"],
+      title: "Mozzarello",
+      stack: ["Javascript", "React", "MongoDB", "Mongoose(koa)"],
       comment: "Trello 커버 프로젝트",
       link: [
-        ["https://github.com/kimtj123/Mozzarello", faGithub],
-        ["https://www.notion.so/Mozzarello-96f0734b4e2148a290cf787c6649062e", faLink],
-        ["https://blog.naver.com/kimtajung1/221744290215", faBlogger]
+        "https://github.com/kimtj123/Mozzarello",
+        "https://www.notion.so/Mozzarello-96f0734b4e2148a290cf787c6649062e",
+        "https://blog.naver.com/kimtajung1/221744290215"
       ],
       gif: [MozzaBoard, MozzaDelete]
     },
     MTOD: {
-      title: "M-TOD",
       term: "2019.11 ~ 2019.12 / 4주",
+      title: "M-TOD",
       stack: ["Javascript", "React-Native", "Native-Base"],
-      comment: "온라인 콘서트 티켓 발권을 서비스하는 어플(Git 소스코드 공개 불가)",
+      comment: "온라인 콘서트 티켓 발권 서비스",
       link: [
-        ["https://bit.ly/36cHJ9g", faLink],
-        ["https://blog.naver.com/kimtajung1/221734201049", faBlogger]
+        "https://bit.ly/36cHJ9g",
+        "https://blog.naver.com/kimtajung1/221734201049"
       ],
       gif: [MTOD]
     },
     farmers: {
-      title: "Farmers",
       term: "2019.10 ~ 2019.11 / 2주",
-      stack: ["Javascript", "React", "HTML", "CSS"],
-      comment: "위치·날씨 정보를 기반으로 관심작물의 상태와 정보 표시",
+      title: "Farmers",
+      stack: ["Javascript", "React"],
+      comment: "위치·날씨 정보를 기반으로n관심작물의 상태와 정보 표시",
       link: [
-        ["https://github.com/codestates/Farmers-client", faGithub],
-        ["https://www.notion.so/5-Farmers-Botanists-04917b9e582f43b982353d3db0a9498d", faLink],
-        ["https://blog.naver.com/kimtajung1/221694557904", faBlogger]
+        "https://github.com/codestates/Farmers-client",
+        "https://www.notion.so/5-Farmers-Botanists-04917b9e582f43b982353d3db0a9498d",
+        "https://blog.naver.com/kimtajung1/221694557904"
       ],
       gif: [Farmers]
     }
@@ -72,13 +70,10 @@ function Project() {
         <p>Project</p>
       </div>
       <div className="project-content">
-        {/* {
+        {
           Object.values(projectInfo).map((project, i) =>
-            <ProjectList project={project} key={`projectList${i}`} />)
-        } */}
-        <Card />
-        <Card />
-        <Card />
+            <Card info={project} index={i} key={`cardList${i}`} />)
+        }
       </div>
     </div>
   );
--- src/sub-component/card.tsx
+++ src/sub-component/card.tsx
@@ -1,27 +1,86 @@
 import React from 'react';
 import '../scss/card.scss';
 
+function multipleElements() {
 
-function Card() {
+  let elements = [];
+  for (let i = 0; i < 20; i++) {
+    elements.push(
+      <div key={i}> element{i + 1} </div>
+    )
+  }
+  return elements;
+}
 
+//this function will separate each four element to display 
+function separateElement() {
+  var separateElements = [];
+  var multiElements = multipleElements();
+
+  for (var i = 0; i < multiElements.length; i += 4) {
+    var oneRow = [];
+    oneRow.push(multiElements.slice(i, i + 4).map(item => {
+      return <div style={{ display: 'inline-block' }}>{item}</div>
+    }))
+    separateElements.push(oneRow.map(itm => { return <div>{itm}</div> }))
+  }
+  return separateElements;
+}
+
+
+
+function Card(props: any, index: number) {
+  let result = props.info.comment.split('n');
   return (
     <div className="card">
       <div className="card-image"></div>
       <div className="card-text">
-        <span className="date">4 days ago</span>
-        <h2>Post One</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
+        <span className={`date font${props.index}`} >{props.info.term}</span>
+        <h2>{props.info.title}</h2>
+        {
+          // 스택 부분
+          props.info.stack.map((val: string, i: number, stack: any) => {
+            if (i % 2 === 1 || i === stack.length - 1) {
+              return (<p>{`#${stack[i - 1]}` + ' ' + `#${stack[i]}`}</p>)
+            }
+          })
+        }
+        {result.map((val: string) => <p className={`comment`}>
+          {val} <br />
+        </p>)}
+        <ul>
+          <li>내용 1</li>
+          <li>내용 2</li>
+          <li>내용 3</li>
+        </ul>
       </div>
-      <div className="card-stats">
-        <div className="stat">
-          <div className="value">Github</div>
+      <div className={`card-button card${props.index}`}>
+        <a href={props.info.link[0]}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="button">
+            <div className="value">Github</div>
+          </button>
+        </a>
+        <div className="button-middle">
+          <a href={props.info.link[1]}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="button border">
+              <div className="value">Blog</div>
+            </button>
+          </a>
         </div>
-        <div className="stat border">
-          <div className="value">Blog</div>
-        </div>
-        <div className="stat">
-          <div className="value">Site</div>
-        </div>
+        <a href={props.info.link[2]}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="button" onClick={() => console.log("Site")}>
+            <div className="value">Site</div>
+          </button>
+        </a>
       </div>
     </div>
 
